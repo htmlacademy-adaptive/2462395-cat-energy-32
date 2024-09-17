3a008d276c12abd9a6f3c8d9bc2c16460e085160
KatiShalmina <e.shalmina@gmail.com>
2024-09-17 22:20:24 +0200

    Добавляет декоративные элементы (#9)
    * Добавляет декоративные элементы
    
    * Добавляет декоративные элементы
    
    * Добавляет декоративные элементы
    
    * Добавляет декор
    
    * Добавляет декор
    
    * Добавляет декор
    
    * Добавляет декор
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки
    
    * Вносит правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
-/* в этот файл добавляет скрипты*/
+const mainNavList = document.querySelector('.main-nav__list');
+const mainHeaderToggle = document.querySelector('.main-header__navigation-toggle');
+
+mainNavList.classList.remove('main-nav__list--nojs');
+mainHeaderToggle.classList.remove('main-header__navigation-toggle--nojs');
+
+mainHeaderToggle.addEventListener('click', () => {
+  if (mainNavList.classList.contains('main-nav__list--open') && mainHeaderToggle.classList.contains('burger-button--close')) {
+    mainNavList.classList.remove('main-nav__list--open');
+    mainHeaderToggle.classList.remove('burger-button--close');
+  } else {
+    mainNavList.classList.add('main-nav__list--open');
+    mainHeaderToggle.classList.add('burger-button--close');
+  }
+});
